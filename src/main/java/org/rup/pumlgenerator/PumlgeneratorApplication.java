b071b668b22429143ac1d25ef56cb4de71f8f598
plfgavilan <pedrocantoria@gmail.com>
2020-09-01 23:05:50 +0200

    To Command Line application

--- src/main/java/org/rup/pumlgenerator/PumlgeneratorApplication.java
+++ src/main/java/org/rup/pumlgenerator/PumlgeneratorApplication.java
@@ -1,13 +1,22 @@
 package org.rup.pumlgenerator;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PumlgeneratorApplication {
+@Log4j2
+public class PumlgeneratorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PumlgeneratorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("EXECUTING : command line runner");
+		log.info("Reading from: " + args[0]);
+
+	}
 }
