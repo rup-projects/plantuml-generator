8297f8e4a92108ceb6a334add5cbecf323909992
plfgavilan <pedrocantoria@gmail.com>
2020-09-03 19:57:49 +0200

    Remove not used unit test

--- src/main/java/org/rup/pumlgenerator/controllers/pumlinterpreter/PumlClassInterpreter.java
+++ src/main/java/org/rup/pumlgenerator/controllers/pumlinterpreter/PumlClassInterpreter.java
@@ -4,6 +4,7 @@ import org.rup.pumlgenerator.model.PumlClass;
 
 import java.util.List;
 
+// TODO Esto es mas como un pipe de operaciones
 public interface PumlClassInterpreter {
     List<PumlClass> interpret(PumlClassContext context);
 }
